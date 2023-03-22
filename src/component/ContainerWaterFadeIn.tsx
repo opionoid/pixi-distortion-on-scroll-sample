1923d349d6b34f0b1bba3b1d17b1e29f4489594e
Opion <kindled0kindred@icloud.com>
2023-03-22 20:07:22 +0900

    fix: useTickを利用、shockwaveを中心付近から出現させる

--- src/component/ContainerWaterFadeIn.tsx
+++ src/component/ContainerWaterFadeIn.tsx
@@ -13,12 +13,7 @@ export const ContainerWaterFadeIn: FC<ContainerWaterFadeInProps> = ({
 }) => {
   const width = Math.min(window.innerWidth, 800)
   const height = Math.min(window.innerWidth * 3 / 4, 600)
-  const shockwaveFilter = new ShockwaveFilter([0.5, 0.5], {
-    amplitude: 30,
-    wavelength: 160,
-    brightness: 1,
-    radius: -1,
-  });
+  const shockwaveFilter = new ShockwaveFilter([400, 400]);
   const filterTexture = PIXI.Texture.from(
     "https://picsum.photos/id/404/800/600"
   );
@@ -27,21 +22,15 @@ export const ContainerWaterFadeIn: FC<ContainerWaterFadeInProps> = ({
     0
   );
 
-  const intervalRef = useRef<any>();
-  const animationTimeRef = useRef<number>(0);
-  useEffect(() => {
-    intervalRef.current = setInterval(() => {
-      animationTimeRef.current += 0.5;
-      shockwaveFilter.time += 0.1;
-      if (shockwaveFilter.time > 3) shockwaveFilter.time = 0;
-      displacementFilter.scale.x = 50 * Math.sin(animationTimeRef.current);
-      displacementFilter.scale.y = 50 * Math.cos(animationTimeRef.current);
-    }, 300);
-    return () => clearInterval(intervalRef.current);
-  }, []);
+  useTick((delta, ticker) => {
+    shockwaveFilter.time += 0.02 * delta;
+    if (shockwaveFilter.time > 3) shockwaveFilter.time = 0;
+    displacementFilter.scale.x = 50 * Math.sin(0.001 * ticker.lastTime);
+    displacementFilter.scale.y = 50 * Math.cos(0.001 * ticker.lastTime);
+  })
 
   return (
-    <Container filters={[shockwaveFilter, displacementFilter] as Filter[]}>
+    <Container width={width + 40} filters={[shockwaveFilter, displacementFilter] as Filter[]}>
       <Sprite texture={PIXI.Texture.WHITE} position={{ x: 20, y: 20 }} width={width - 40} height={height - 40} alpha={0} />
       <Sprite image="https://picsum.photos/id/439/812/600" x={40} y={40} width={width - 80} height={height - 80} />
     </Container>
